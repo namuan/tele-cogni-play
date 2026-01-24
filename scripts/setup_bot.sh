5868ba2698ed5c477285212bbe1be57d503326e8
namuan <575441+namuan@users.noreply.github.com>
2026-01-24 11:12:39 +0000

    feat(script): update setup and stop commands to support tele-cogni-play

--- scripts/setup_bot.sh
+++ scripts/setup_bot.sh
@@ -1,3 +1,3 @@
 cd $1 || exit
 uv sync --no-dev
-bash ./scripts/start_screen.sh "$1" 'uv run python -m cogniplay.main'
+bash ./scripts/start_screen.sh tele-cogni-play 'uv run python -m cogniplay.main'
--- scripts/stop_bot.sh
+++ scripts/stop_bot.sh
@@ -1,2 +1,2 @@
 cd $1 || exit
-bash ./scripts/stop_screen.sh "$1"
+bash ./scripts/stop_screen.sh tele-cogni-play
